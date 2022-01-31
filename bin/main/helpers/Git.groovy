bcddfcd0c6bf97e4bcf180eac64d1fb018f5389d
victor <victor.menares.sapiain@gmail.com>
2022-01-31 20:49:07 -0300

    se agrega merge

--- bin/main/helpers/Git.groovy
+++ bin/main/helpers/Git.groovy
@@ -1,7 +1,22 @@
 package helpers
 
 def merge(String ramaOrigen, String ramaDestino){
-   println "Este método realiza un merge ${ramaOrigen} y ${ramaOrigen} "
+   println "Este método realiza un merge ${ramaOrigen} y ${ramaDestino} "
+
+    // checkout de la ranaOrigen
+    checkout(ramaOrigen)
+    // checkout de la rama ramaDestino
+    checkout(ramaDestino)
+    
+    // merge ramaOrigen
+    sh  """
+        git merge ${ramaOrigen}
+        git commit -m "merge de  ${ramaOrigen}"
+        git push origin ${ramaDestino}
+
+
+    """
+   
 }
 
 def tag(String ramaOrigen){
@@ -10,6 +25,10 @@ def tag(String ramaOrigen){
         -validar que la rama release cumpla con un patron o una nomenclatura definida release-v(major)-(minor)-(patch)
         -obtener el valor desde la "v" en adelanre, ejemplo release--V1-0-0 ... obtener el tag 1-0-0 
     */
+}
+
+def checkout(String rama){
+      sh "git reset --hard head; git checkout origin  ${rama}; git pull origin ${rama} "
 }
 
 return this;
