f0c8a0686cf8f81294e742fb8d3e361aa1bd9845
victor <victor.menares.sapiain@gmail.com>
2022-01-31 21:50:14 -0300

    se agrega helpers

--- src/helpers/Git.groovy
+++ src/helpers/Git.groovy
@@ -1,49 +1,35 @@
 package helpers
 
 def merge(String ramaOrigen, String ramaDestino){
-   println "Este método realiza un merge ${ramaOrigen} y ${ramaDestino} "
+	println "Este método realiza un merge ${ramaOrigen} y ${ramaDestino}"
 
-    // checkout de la ranaOrigen
-    checkout(ramaOrigen)
-    // checkout de la rama ramaDestino
-    checkout(ramaDestino)
-    
-    // merge ramaOrigen
-    sh  """
-        git merge ${ramaOrigen}
-        git push origin ${ramaDestino}
+	checkout(ramaOrigen)
+	checkout(ramaDestino)
 
-    """
-   
+	sh """
+		git merge ${ramaOrigen}
+		git push origin ${ramaDestino}
+	"""
 }
 
 def tag(String ramaOrigen, String ramaDestino){
-    println "este método realiza un tag ${ramaOrigen}"
+	println "Este método realiza un tag en master de ${ramaOrigen}"
 
-    if (ramaOrigen.contains('release-v')){
-        // cortar la variable y dejar solamente v1-0-0
-        checkout(ramaDestino)
-        def tagValue = ramaOrigen.split('release-v')[1] // {['','1-0-0']}
+	if (ramaOrigen.contains('release-v')){
+		checkout(ramaDestino)
+		def tagValue = ramaOrigen.split('release-')[1]
+		sh """
+			git tag ${tagValue}
+			git push origin ${tagValue}
+		"""
 
-        tagValue.each{println it}
-
-        sh """
-        git tag ${tagValue}
-        git push origin ${tagValue}
-
-        """
-
-    }else{
-        error "la rama ${ramaOrigen} no cumple con nomenclatura definida param rama release-v(major)-(minor)-(patch) "
-    }
-    /*
-        -validar que la rama release cumpla con un patron o una nomenclatura definida release-v(major)-(minor)-(patch)
-        -obtener el valor desde la "v" en adelanre, ejemplo release--V1-0-0 ... obtener el tag 1-0-0 
-    */
+	} else {
+		error "La rama ${ramaOrigen} no cumple con nomenclatura definida para rama release (release-v(major)-(minor)-(patch))."
+	}
 }
 
 def checkout(String rama){
-      sh "git reset --hard HEAD; git checkout ${rama}; git pull origin ${rama} "
+	sh "git reset --hard HEAD; git checkout ${rama}; git pull origin ${rama}"
 }
 
 return this;
